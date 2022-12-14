245fad0473d04fa2e41d34892a31711f4f90d686
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2022-12-13 21:59:37 -0800

    v1_presentation batch print (part 2)

--- src/pages/logistics/outbound-page/components/CustomerOrderPrint.tsx
+++ src/pages/logistics/outbound-page/components/CustomerOrderPrint.tsx
@@ -1,8 +1,9 @@
 import { useReactToPrint } from "react-to-print";
 import { useRef, useState, useEffect } from "react";
 import { BiPrinter, BiX } from "react-icons/bi";
-import { convertTime } from "../../../../commons/time.util";
 import NumberInput from "../../../../components/NumberInput";
+import PalletLabelToPrint from "./PalletLabelToPrint";
+import PackingSlipToPrint from "./PackingSlipToPrint";
 
 export default function CustomerOrderPrint({ order }) {
   const [pallet, setPallet] = useState({count: 1, list: [null]});
@@ -34,44 +35,8 @@ export default function CustomerOrderPrint({ order }) {
   return (
   <>
     <div className="hidden">
-      {/* Customer Order to print */}
-      <div ref={orderToPrintRef} className="px-6 py-4">
-        <div className="flex flex-col">
-          <div className="mb-8">
-            <h1 className="block text-sm font-serif">New San Jose Wholesale Foods, Inc.</h1>
-            <p className="block text-xs">1005 S. 5th St, San Jose, CA 95112</p>
-            <p className="block text-xs">Mailing Address: P.O. Box 730967, San Jose, CA 95173 - 0967</p>
-            <p className="block text-xs">Tel: (408) 279-3888 • (408) 279-3889 • (408) 279-0413 • Fax: (408) 279-3890</p>
-          </div>
-          <div className="mb-8 ml-24">
-            <div className="block text-2xl">#{order.code}</div>
-            <div className="text-4xl font-bold block">{order.customerName}</div>
-            <div className="text-2xl mt-2">Ship at: {convertTime(new Date(order.expectedAt))}</div>
-          </div>
-          <div className="flex pb-1 w-9/12 border-black border-b-4 ml-24">
-            <div className="font-semibold w-8/12">Item Description</div>
-            <div className="font-semibold w-4/12 text-center">Quantity</div>
-          </div>
-          {order.productCustomerOrders.map(productOrder => (
-          <div key={productOrder.productName} className="flex py-2 w-9/12 border-black border-b ml-24">
-            <div className="w-8/12">{productOrder.productName}</div>
-            <div className="w-4/12 text-center font-semibold">{productOrder.quantity}</div>
-          </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Pallet to print */}
-      <div ref={palletLabelToPrintRef}>
-        {pallet.list.map((item, index) => (
-        <div className="h-screen px-6" key={`pallet-label-${index}`}>
-          <div className="block text-2xl">#{order.code}</div>
-          <div className="text-4xl font-bold block">{order.customerName}</div>
-          <div className="text-2xl block mt-2">Ship at: {convertTime(new Date(order.expectedAt))}</div>
-          <div className="block text-sm mt-2">Page {index + 1} of {pallet.list.length}</div> 
-        </div>
-        ))}        
-      </div>
+      <PackingSlipToPrint printRef={orderToPrintRef} order={order} />
+      <PalletLabelToPrint printRef={palletLabelToPrintRef} pallet={pallet} order={order} />
     </div>
 
     {/* Pallet modal */}
