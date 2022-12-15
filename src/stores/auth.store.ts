c8d2eb006dd28d02bc3df856118330562476cd0d
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2022-12-15 14:52:01 -0800

    v1_presentation fix #21

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -10,23 +10,23 @@ interface AuthState {
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-  username: sessionStorage.getItem("username"),
-  role: +sessionStorage.getItem("role"),
+  username: localStorage.getItem("username"),
+  role: +localStorage.getItem("role"),
   token: localStorage.getItem("token"),
   signIn: (resData: SignInResponse) => {
-    sessionStorage.removeItem("username");
-    sessionStorage.removeItem("role");
+    localStorage.removeItem("username");
+    localStorage.removeItem("role");
     localStorage.removeItem("token");
-    sessionStorage.setItem("username", JSON.stringify(resData.username));
-    sessionStorage.setItem("role", JSON.stringify(resData.roleId));
+    localStorage.setItem("username", JSON.stringify(resData.username));
+    localStorage.setItem("role", JSON.stringify(resData.roleId));
     localStorage.setItem("token", resData.token);
-    set({ username: sessionStorage.getItem("username") });
-    set({ role: +sessionStorage.getItem("role") });
+    set({ username: localStorage.getItem("username") });
+    set({ role: +localStorage.getItem("role") });
     set({ token: localStorage.getItem("token") });
   },
   signOut: () => {
-    sessionStorage.removeItem("username");
-    sessionStorage.removeItem("role");
+    localStorage.removeItem("username");
+    localStorage.removeItem("role");
     localStorage.removeItem("token");
     set({ username: null });
     set({ role: null });
