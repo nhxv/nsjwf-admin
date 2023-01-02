5ea25f2d15aa4fcb583abfd60306a9cd752f04f1
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-01-02 15:44:16 -0800

    v1_presentation full content print

--- src/pages/customer/view-customer-order-page/components/CustomerOrderPrint.tsx
+++ src/pages/customer/view-customer-order-page/components/CustomerOrderPrint.tsx
@@ -31,10 +31,10 @@ export default function CustomerOrderPrint({ order }) {
       if (printerStatus.isReadyToPrint) {
         for (let i = 0; i < pallet.count; i++) {
           const code = `^FO50,50^ADN,36,20^FD${order.code}^FS`;
-          const customerName = `^FO10,50^ADN,36,20^FD${order.customerName}^FS`;
-          const date = `^FO150,50^ADN,36,20^FD${convertTime(new Date(order.expectedAt))}^FS`;
-          const page = `^FO200,50^ADN,36,20^FD${i+1}^FS`;
-          const zpl = `^XA` + customerName + `^XZ`;
+          const customerName = `^FO50,100^ADN,36,20^FD${order.customerName}^FS`;
+          const date = `^FO50,150^ADN,36,20^FD${convertTime(new Date(order.expectedAt))}^FS`;
+          const page = `^FO50,200^ADN,36,20^FD${i+1}^FS`;
+          const zpl = `^XA` + code + customerName + date + page + `^XZ`;
           browserPrint.print(zpl);
         }
       }
