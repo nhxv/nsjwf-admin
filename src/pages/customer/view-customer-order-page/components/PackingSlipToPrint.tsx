ca7a760ec32ad9c731c3ad884c38337664cf6b77
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-01-04 15:34:23 -0800

    v1_presentation change packing slip format

--- src/pages/customer/view-customer-order-page/components/PackingSlipToPrint.tsx
+++ src/pages/customer/view-customer-order-page/components/PackingSlipToPrint.tsx
@@ -2,18 +2,26 @@ import { convertTime } from "../../../../commons/time.util";
 
 export default function PackingSlipToPrint({ printRef, order }) {
   return (
-  <div ref={printRef} className="px-6 py-4 break-after-page">
+  <div ref={printRef} className="px-6 py-2 break-after-page">
     <div className="flex flex-col">
-      <div className="mb-8">
-        <h1 className="block text-[0.5rem] font-serif">New San Jose Wholesale Foods, Inc.</h1>
-        <p className="block text-[0.375rem]">1005 S. 5th St, San Jose, CA 95112</p>
-        <p className="block text-[0.375rem]">Mailing Address: P.O. Box 730967, San Jose, CA 95173 - 0967</p>
-        <p className="block text-[0.375rem]">Tel: (408) 279-3888 • (408) 279-3889 • (408) 279-0413 • Fax: (408) 279-3890</p>
+      <div className="mb-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="block text-[0.625rem] font-serif">New San Jose Wholesale Foods, Inc.</h1>
+            <p className="block text-[0.5rem]">1005 S. 5th St, San Jose, CA 95112</p>
+            <p className="block text-[0.5rem]">Mailing Address: P.O. Box 730967, San Jose, CA 95173 - 0967</p>
+            <p className="block text-[0.5rem]">Tel: (408) 279-3888 • (408) 279-3889 • (408) 279-0413 • Fax: (408) 279-3890</p>
+          </div>
+          <div>
+            <p className="text-xs"><span className="font-semibold">Invoice No.:</span> {`327353`}</p>
+            <p className="text-xs"><span className="font-semibold">Ship at:</span> {convertTime(new Date(order.expectedAt))}</p>
+          </div>
+        </div>
+
       </div>
       <div className="mb-8 ml-24">
-        <div className="block text-2xl">#{order.code}</div>
+        <div className="block text-2xl">Packing Slip</div>
         <div className="text-4xl font-bold block">{order.customerName}</div>
-        <div className="text-2xl mt-2">Ship at: {convertTime(new Date(order.expectedAt))}</div>
       </div>
       <div className="flex pb-1 w-9/12 border-black border-b-4 ml-24">
         <div className="font-semibold w-8/12">Item Description</div>
