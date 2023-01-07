66566c3f543cd2776fced993ab7ba8b489f50f5c
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-01-06 20:28:17 -0800

    v1_tendency smaller pallet print

--- src/pages/customer/view-customer-order-page/components/PalletLabelToPrint.tsx
+++ src/pages/customer/view-customer-order-page/components/PalletLabelToPrint.tsx
@@ -4,12 +4,12 @@ export default function PalletLabelToPrint({ printRef, pallet, order }) {
   return (
     <div ref={printRef}>
     {pallet.list.map((_, index) => (
-    <div className="flex justify-center mt-4 break-after-page" key={`pallet-label-${index}`}>
+    <div className="flex break-after-page" key={`pallet-label-${index}`}>
       <div>
         {/* <div className="block text-2xl">#{order.code}</div> */}
-        <div className="text-xl font-bold block">{order.customerName}</div>
+        <div className="text-[4px] font-bold block">{order.customerName}</div>
         {/* <div className="text-xl block mt-2">Ship at: {convertTime(new Date(order.expectedAt))}</div> */}
-        <div className="block text-sm">Page {index + 1} of {pallet.list.length}</div> 
+        <div className="block text-[4px]">Page {index + 1} of {pallet.list.length}</div> 
       </div>
 
     </div>
