47c7e9dac48b70e7e7eaaf579ae1039c9c7e019c
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-02-06 01:47:22 -0800

    v1_misc report manual code (#68)
    * v1_misc refactor jsx
    
    * v1_misc fix #60
    
    * v1_misc attempt to fix #17
    
    * v1_misc update operator bottom nav
    
    * v1_misc display error when re-assign on-doing task
    
    * v1_misc display error when start task assigned to someone else
    
    * v1_misc improve form accessibility
    
    * v1_misc refactor model & add manual code
    
    * v1_misc report manual code

--- src/pages/customer/report-customer-sale-page/ReportCustomerSalePage.tsx
+++ src/pages/customer/report-customer-sale-page/ReportCustomerSalePage.tsx
@@ -65,7 +65,7 @@ export default function ReportCustomerSalePage() {
 
   const onDownloadReport = () => {
     const reportData = data.reports.filter(report => report.is_test).map(report => ({
-      code: `#${report.order_code}`,
+      code: `#${report.manual_code ? report.manual_code : report.order_code}`,
       refund_code: `#${report.refund_order}`,
       customer: report.customer_name,
       date: convertTime(new Date(report.date)),
@@ -82,38 +82,36 @@ export default function ReportCustomerSalePage() {
   }
 
   return (
-    <>
-      <section className="min-h-screen">
-        <div className="flex flex-col items-center">
-          <div className={`my-6`}>
-            {(!listState.listEmpty && !listState.listError && !listState.listLoading) ? (            
-            <div className="text-end">
-              <button type="button" className="btn btn-accent" onClick={onDownloadReport}>
-                <span className="mr-2">Download report</span>
-                <BiDownload className="w-6 h-6"></BiDownload>
-              </button>
-            </div>
-            ): (<></>)}
+    <section className="min-h-screen">
+      <div className="flex flex-col items-center">
+        <div className={`my-6`}>
+          {(!listState.listEmpty && !listState.listError && !listState.listLoading) ? (            
+          <div className="text-end">
+            <button type="button" className="btn btn-accent" onClick={onDownloadReport}>
+              <span className="mr-2">Download report</span>
+              <BiDownload className="w-6 h-6"></BiDownload>
+            </button>
           </div>
-
-          {listState.listLoading ? (
-          <Spinner></Spinner>
-          ) : (
-          <div className="w-11/12 sm:w-8/12 xl:w-6/12">
-            {listState.listError ? (
-            <Alert message={listState.listError} type="error"></Alert>
-            ) : (
-            <>
-              {listState.listEmpty ? (
-              <Alert message={listState.listEmpty} type="empty"></Alert>
-              ) : (
-              <CustomerSaleList reports={data.display} />
-              )}
-            </>
-            )}
-          </div>)}    
+          ): null}
         </div>
-      </section>
-    </>
+
+        {listState.listLoading ? (
+        <Spinner></Spinner>
+        ) : (
+        <div className="w-11/12 sm:w-8/12 xl:w-6/12">
+          {listState.listError ? (
+          <Alert message={listState.listError} type="error"></Alert>
+          ) : (
+          <>
+            {listState.listEmpty ? (
+            <Alert message={listState.listEmpty} type="empty"></Alert>
+            ) : (
+            <CustomerSaleList reports={data.display} />
+            )}
+          </>
+          )}
+        </div>)}    
+      </div>
+    </section>
   )
 }
