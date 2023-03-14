7cececf75b290dceda5bc9e37ffa742b7f5cc3d3
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-03-14 12:24:38 -0700

    v1_misc change packing slip font size (#117)
    * v1_misc refactor jsx
    
    * v1_misc fix #60
    
    * v1_misc attempt to fix #17
    
    * v1_misc update operator bottom nav
    
    * v1_misc display error when re-assign on-doing task
    
    * v1_misc display error when start task assigned to someone else
    
    * v1_misc improve form accessibility
    
    * v1_misc refactor model & add manual code
    
    * v1_misc report manual code
    
    * v1_misc sell price suggest
    
    * v1_misc add headless modal and refactor shared components
    
    * v1_misc darkness evolve
    
    * v1_misc darker
    
    * v1_misc steal from hell
    
    * v1_misc back to tailwind dungeon
    
    * v1_misc update employee account
    
    * v1_unit update product config view (#76)
    
    * v1_unit improve customer & vendor config view (#78)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement precommit hook (#80)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement precommit hook
    
    * v1_unit fix format (#82)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement precommit hook
    
    * v1_unit implement tailwind format
    
    * v1_unit fix format
    
    * v1_unit fix print width (#84)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement precommit hook
    
    * v1_unit implement tailwind format
    
    * v1_unit fix format
    
    * v1_unit fix print width
    
    * v1_unit implement product units (#86)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement precommit hook
    
    * v1_unit implement tailwind format
    
    * v1_unit fix format
    
    * v1_unit fix print width
    
    * v1_unit implement product units
    
    * v1_unit reformat unit
    
    * v1_reality cleanup legacy logic flow (#88)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix formatting
    
    * v1_payment implement customer payment (#92)
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment cleanup format (#94)
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment fix jsx condition
    
    * v1_payment cleanup format
    
    * v1_payment fix total price calc (#102)
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment fix jsx condition
    
    * v1_payment cleanup format
    
    * v1_payment fix total price #100
    
    * v1_payment fix format
    
    * v1_payment fix refund deduction
    
    * v1_misc add titles to page
    
    * v1_misc fix daily page title padding
    
    * v1_misc fix daily page title padding
    
    * v1_misc fix daily page title padding
    
    * v1_misc finalize title padding
    
    * v1_misc improve UI
    
    * v1_misc improve UI
    
    * v1_misc fix stock flow
    
    * v1_misc fix format
    
    * v1_misc update employee credentials
    
    * v1_misc fix format
    
    * v1_misc change order time
    
    * v1_payment revert customer order
    
    * v1_payment fix format
    
    * v1_misc not display box unit
    
    * v1_misc fix format
    
    * v1_misc fix sale type
    
    * v1_misc change packing slip font size

--- src/pages/customer/view-customer-order-page/components/PackingSlipToPrint.tsx
+++ src/pages/customer/view-customer-order-page/components/PackingSlipToPrint.tsx
@@ -50,13 +50,13 @@ export default function PackingSlipToPrint({ printRef, order }) {
             key={productOrder.productName}
             className="ml-24 flex w-9/12 border-b border-black py-2"
           >
-            <div className="w-[64px] text-xs font-semibold">
+            <div className="w-[64px] text-sm font-semibold">
               {productOrder.quantity}{" "}
               {productOrder.unitCode === "box"
                 ? ``
                 : `(${productOrder.unitCode})`}
             </div>
-            <div className="ml-8 text-xs">{productOrder.productName}</div>
+            <div className="ml-8 text-sm">{productOrder.productName}</div>
           </div>
         ))}
       </div>
