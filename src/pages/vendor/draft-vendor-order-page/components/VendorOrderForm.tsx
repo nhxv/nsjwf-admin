nhxv/nsjwf-admin

commit 55d89633813b04d00de34ccc87ee7e6ebf04ebc2

Add attachment loading

src/pages/vendor/draft-vendor-order-page/components/VendorOrderForm.tsx
@@ -466,6 +466,10 @@ export default function VendorOrderForm({
                         <span>Click to view attachment</span>
                       </div>
                     </div>
+                  ) : vendorOrderForm.values.attachmentExists ? (
+                    <div className="custom-card sticker-primary relative w-full text-center dark:border-2">
+                      <Spinner />
+                    </div>
                   ) : (
                     <div className="w-full">
                       <FileInput
@@ -535,7 +539,8 @@ export default function VendorOrderForm({
                     disabled={
                       initialData.status === "COMPLETED" ||
                       formState.loading ||
-                      vendorOrderForm.isSubmitting
+                      vendorOrderForm.isSubmitting ||
+                      (vendorOrderForm.values.attachmentExists && !imageURL)
                     }
                   >
                     <span>{edit ? "Update" : "Create"}</span>

src/pages/vendor/draft-vendor-order-page/components/VendorOrderFormContainer.tsx
@@ -155,6 +155,7 @@ export default function VendorOrderFormContainer() {
               manualCode: orderRes.data.manual_code ?? "",
               expectedAt: convertTime(new Date(orderRes.data.expected_at)),
               attachment: null,
+              attachmentExists: !!attachmentPromise,
               ...productFieldData,
             }));
 
@@ -175,6 +176,7 @@ export default function VendorOrderFormContainer() {
                     setInitialFields((prev) => ({
                       ...prev,
                       attachment: file,
+                      attachmentAvailable: true,
                     }));
                   }
                 })

commit a244d89a120a5d4e31bcb7358446d79f24d83231

Fix bug caused from 55d89633

src/pages/vendor/draft-vendor-order-page/components/VendorOrderForm.tsx
@@ -453,6 +453,10 @@ export default function VendorOrderForm({
                           e.stopPropagation(); // Stop propagation to div
 
                           vendorOrderForm.setFieldValue("attachment", null);
+                          vendorOrderForm.setFieldValue(
+                            "attachmentExists",
+                            false
+                          );
                         }}
                       >
                         <span>
@@ -501,6 +505,10 @@ export default function VendorOrderForm({
                               vendorOrderForm.setFieldValue(
                                 "attachment",
                                 compressedFile
+                              );
+                              vendorOrderForm.setFieldValue(
+                                "attachmentExists",
+                                true
                               );
                             } catch (error) {
                               setFormState((prev) => ({